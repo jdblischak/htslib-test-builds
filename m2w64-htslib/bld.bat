05a6b7e6b94c4d52fd1fb1ae6b479050138b33a0
John Blischak <jdblischak@gmail.com>
2023-02-01 16:07:57 -0500

    Improve bld.bat

--- m2w64-htslib/bld.bat
+++ m2w64-htslib/bld.bat
@@ -1,3 +1,10 @@
-./configure --prefix=%LIBRARY_PREFIX%
+@echo on
+
+bash configure --prefix=%LIBRARY_PREFIX%
+IF %ERRORLEVEL% NEQ 0 exit 1
+
 make
+IF %ERRORLEVEL% NEQ 0 exit 1
+
 make install
+IF %ERRORLEVEL% NEQ 0 exit 1
