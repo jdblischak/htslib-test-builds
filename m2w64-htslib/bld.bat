994abcb11cc4da8f4462ce0c4bf08607dd9c962f
John Blischak <jdblischak@gmail.com>
2023-02-02 12:28:04 -0500

    Use gcc flags from AppVeyor build
    https://github.com/samtools/htslib/blob/463830bf7de8c4ab731c4d67c49ddc446f498f50/.appveyor.yml#L37

--- m2w64-htslib/bld.bat
+++ m2w64-htslib/bld.bat
@@ -1,6 +1,6 @@
 @echo on
 
-bash configure --prefix=%LIBRARY_PREFIX% --disable-lzma
+bash configure --prefix=%LIBRARY_PREFIX% --disable-lzma --enable-werror CFLAGS='-g -O3'
 IF %ERRORLEVEL% NEQ 0 exit 1
 
 make
