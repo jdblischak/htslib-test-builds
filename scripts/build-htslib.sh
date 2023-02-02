a766bba33d27b7be59bd528e2d7da0a92394074c
John Blischak <jdblischak@gmail.com>
2023-02-02 16:08:22 -0500

    Use patches

--- scripts/build-htslib.sh
+++ scripts/build-htslib.sh
@@ -1,24 +1,11 @@
 LIBHTS_SOVERSION=${LIBHTS_SOVERSION-3}
 
-ls -lh config.mk*
-
 ./configure CFLAGS=-DCURL_STATICLIB
 
-ls -lh config.mk*
-
-# download patches
-wget https://raw.githubusercontent.com/TileDB-Inc/TileDB-VCF/dlh/sc-24772-msys2-htslib-msvc-tdbvcf-WIP1/libtiledbvcf/cmake/patches/htslib.1.15.1.Makefile.staticlink
-wget https://raw.githubusercontent.com/TileDB-Inc/TileDB-VCF/dlh/sc-24772-msys2-htslib-msvc-tdbvcf-WIP1/libtiledbvcf/cmake/patches/htslib.1.15.1.config.mk.staticlink
-
-# diff patches
-diff Makefile htslib.1.15.1.Makefile.staticlink
-diff config.mk htslib.1.15.1.config.mk.staticlink
-
 # apply patches
-cp htslib.1.15.1.Makefile.staticlink Makefile
-cp htslib.1.15.1.config.mk.staticlink config.mk
+patch Makefile ci/patches/makefile.staticlink.patch
+patch config.mk ci/patches/config.mk.staticlink.patch
 
-# build with patches
 make
 
 ls -lh hts-${LIBHTS_SOVERSION}.*
