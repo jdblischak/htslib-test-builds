7d4caa39c826e321672530b980608b892f3275f6
John Blischak <jdblischak@gmail.com>
2023-02-03 14:29:36 -0500

    the target directories are all empty

--- m2w64-htslib-dll/bld.bat
+++ m2w64-htslib-dll/bld.bat
@@ -1,15 +1,13 @@
 @echo on
 
-robocopy bin %LIBRARY_PREFIX%\bin *.dll
-IF %ERRORLEVEL% NEQ 3 exit 1
 rem robocopy exit codes
 rem https://learn.microsoft.com/en-us/troubleshoot/windows-server/backup-and-storage/return-codes-used-robocopy-utility
 
+robocopy bin %LIBRARY_PREFIX%\bin *.dll
+IF %ERRORLEVEL% NEQ 1 exit 1
+
 robocopy lib %LIBRARY_PREFIX%\lib *.lib
-IF %ERRORLEVEL% NEQ 3 exit 1
+IF %ERRORLEVEL% NEQ 1 exit 1
 
 robocopy include %LIBRARY_PREFIX%\include /E
-IF %ERRORLEVEL% NEQ 3 exit 1
-
-dir %LIBRARY_PREFIX%\include
-dir %LIBRARY_PREFIX%\include\htslib
+IF %ERRORLEVEL% NEQ 1 exit 1
