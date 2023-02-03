3b27b63158ad008c5fa4b6a7a7a5d442813fd91c
John Blischak <jdblischak@gmail.com>
2023-02-03 13:03:10 -0500

    Explicitly copy from %SRC_DIR%

--- m2w64-htslib-dll/bld.bat
+++ m2w64-htslib-dll/bld.bat
@@ -1,18 +1,21 @@
 @echo on
 
-copy hts-3.dll %LIBRARY_PREFIX%\bin
-copy htslib/hts_defs.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/hts_endian.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/hts_expr.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/hts_log.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/hts.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/hts_os.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/vcf.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/vcfutils.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/hfile.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/kstring.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/k*.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/synced_bcf_reader.h %LIBRARY_PREFIX%\include\htslib
-copy htslib/tbx.h %LIBRARY_PREFIX%\include\htslib
-copy hts-3.lib %LIBRARY_PREFIX%\lib
+echo %CD%
+dir
+
+copy %SRC_DIR%\hts-3.dll %LIBRARY_PREFIX%\bin
+copy %SRC_DIR%\htslib/hts_defs.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/hts_endian.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/hts_expr.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/hts_log.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/hts.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/hts_os.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/vcf.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/vcfutils.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/hfile.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/kstring.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/k*.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/synced_bcf_reader.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\htslib/tbx.h %LIBRARY_PREFIX%\include\htslib
+copy %SRC_DIR%\hts-3.lib %LIBRARY_PREFIX%\lib
 IF %ERRORLEVEL% NEQ 0 exit 1
