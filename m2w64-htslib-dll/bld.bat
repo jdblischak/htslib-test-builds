cf22c3cbc0145327b2e1610550e4dbcfdf9d8462
John Blischak <jdblischak@gmail.com>
2023-02-03 12:39:52 -0500

    Tarball is already unpacked

--- m2w64-htslib-dll/bld.bat
+++ m2w64-htslib-dll/bld.bat
@@ -1,4 +1,18 @@
 @echo on
 
-tar xzf m2w64-htslib-1.15.1-0.tar.gz -C %LIBRARY_PREFIX%
+copy hts-3.dll %LIBRARY_PREFIX%\bin
+copy htslib/hts_defs.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/hts_endian.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/hts_expr.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/hts_log.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/hts.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/hts_os.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/vcf.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/vcfutils.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/hfile.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/kstring.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/k*.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/synced_bcf_reader.h %LIBRARY_PREFIX%\include\htslib
+copy htslib/tbx.h %LIBRARY_PREFIX%\include\htslib
+copy hts-3.lib %LIBRARY_PREFIX%\lib
 IF %ERRORLEVEL% NEQ 0 exit 1
